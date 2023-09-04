eb3d8a93ea73d7aa7c0cf5ba3c3a488d929add9c
elmaqwerty14 <elmakiu333@gmail.com>
2023-09-05 02:19:14 +0700

    edit app.js

--- ErrorHandling/app.js
+++ ErrorHandling/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const app = express();
+const app = express ();
 
+function fetchDataMiddleware(req, res, next) {
+    setTimeout(function() {
+        const data = 'ini adalah data yang diambil dari middleware';
+        req.fetchedData = data;
+        next();
+    }, 1000)  
+}
+app.get('/', fetchDataMiddleware, (req, res) => {
+    console.log('Data di route:', req.fetchedData);
+    res.send('Data fetched and handled using middleware');
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 app.listen(3000, () => {
