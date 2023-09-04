702308c69507d2d346fd463dc3f8a2e3901a2e3d
elmaqwerty14 <elmakiu333@gmail.com>
2023-09-05 02:03:04 +0700

    edit app.js

--- MiddelewareExpress/app.js
+++ MiddelewareExpress/app.js
@@ -18,5 +18,5 @@ app.get('/', fetchDataMiddleware, (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
