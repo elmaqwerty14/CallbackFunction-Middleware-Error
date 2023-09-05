43888b3f5974bf3e19c9d34504a56466d83c6b57
elmaqwerty14 <elmakiu333@gmail.com>
2023-09-05 14:14:58 +0700

    update

--- CallbackFunction/app.js
+++ CallbackFunction/app.js
@@ -4,10 +4,11 @@ const app = express();
 
 // membuat function fetchingdata untuk mengambil data
 function fetchData(callback){
+    // "setTimeout", digunakan untuk memanggil fungsi atau mengevaluasi expresi setelah periode waktu yang telah diberikan (dalam satuan milidetik).
     setTimeout(function(){
         const data = 'ini adalah data yang diambil';
         callback(data);
-    }, 1000);
+    }, 1000); // fungsi callback disini tidak memiliki nama dikenal dengan istilah "Anonymous Function" dan akan dipanggil setelah 1 detik dilewati. (1 detik = 1000 milidetik)
 }
 
 // membuat function handledata untuk menampilkan data yang diterima jika data sudah siap
